e042d3f207854e785eca521a49f045eb5ea89e08
Bárbara Lucia Lombardo <barbara.lombardo.98@gmail.com>
2022-09-21 00:11:24 -0300

    feat: Initial project.
    + Server connection.
    + Room creation and joining.
    + Main menu and Game (Room) created.

---
+++ Assets/Scripts/Controller/NetManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class NetManager : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField] Button btnConnection;
+    [SerializeField] TextMeshProUGUI connectionStatus;
+    [SerializeField] TextMeshProUGUI roomName;
+    [SerializeField] TextMeshProUGUI characterNickName;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        btnConnection.interactable = false;
+        connectionStatus.text = "Connecting to Master";
+    }
+    public override void OnConnectedToMaster()
+    {
+        btnConnection.interactable = false;
+        PhotonNetwork.JoinLobby();
+        connectionStatus.text = "Connecting to Lobby";
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        connectionStatus.text = "Connection with master has failed, cause was: " + cause;
+    }
+
+    public override void OnJoinedLobby()
+    {
+        btnConnection.interactable = true;
+        connectionStatus.text = "Connected to Lobby";
+    }
+    public override void OnLeftLobby()
+    {
+        base.OnLeftLobby();
+        connectionStatus.text = "Disconnected from lobby";
+    }
+    public void Connect()
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 3;
+        options.IsOpen = true;
+        options.IsVisible = true;
+        //TODO setear nickname
+
+        Debug.Log("Room name:" + roomName.text);
+
+        PhotonNetwork.JoinOrCreateRoom(roomName.text, options, TypedLobby.Default);
+
+        btnConnection.interactable = false;
+    }
+
+    public override void OnCreatedRoom()
+    {
+        connectionStatus.text = "Room" + roomName.text + " was created";
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        connectionStatus.text = "Failed to create room " + roomName.text;
+        btnConnection.interactable = true;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        connectionStatus.text = "Joined room";
+        PhotonNetwork.LoadLevel(roomName.text);
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        connectionStatus.text = "Failed to join room " + roomName.text;
+        btnConnection.interactable = true;
+    }
+
+}
